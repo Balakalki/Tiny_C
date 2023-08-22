9d398b72e8ea5a77ea0bbe105f29ba6d731baf47
Aluri Bala Kalki <aluribalakalki5@gmail.com>
2023-08-22 17:18:34 +0530

    modified ID as it include numbers also like in c language

--- Tiny_C_Lexer.py
+++ Tiny_C_Lexer.py
@@ -5,7 +5,15 @@ class lex(Lexer):
     ignore = " \n"
     tokens = ("ID","INT","PRINT","CONST")
     CONST = r'[0-9]+'
-    ID = r'[a-zA-Z][a-zA-Z_]*'
+    ID = r'[a-zA-Z][a-zA-Z_0-9]*'
     ID ['print'] = PRINT 
     ID ['int'] = INT
-obj = lex()
+
+# Level1  of Tiny C:
+
+# a) Program consists of only one function main()
+# b) Keywords  :   int, print
+# c) Datatypes :  int 
+# d) Identifiers :  starts with letter followed by one or more digits or underscore
+# e) The statements  allowed are  declaration statement, assignment statement, print statement 
+# f) Print syntax:   print identifier
