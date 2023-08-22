de28a7a6c2c48ec4e0c9c12acc4599b89a5e6fd9
Aluri Bala Kalki <aluribalakalki5@gmail.com>
2023-08-22 16:57:09 +0530

    modified Lexer code to import it into Parser

--- Tiny_C_Lexer.py
+++ Tiny_C_Lexer.py
@@ -1,13 +1,11 @@
 from sly import Lexer
 
 class lex(Lexer):
-    literals = (";","=","==","+","-","*","/","%","!",",","(",")","{","}","[","]","<",">",".")
-    ignore = " "
-    tokens = ("ID","INT","PRINT")
-    INT = r'[0-9]+'
+    literals = (";","=","\n","==","+","-","*","/","%","!",",","(",")","{","}","[","]","<",">",".")
+    ignore = " \n"
+    tokens = ("ID","INT","PRINT","CONST")
+    CONST = r'[0-9]+'
     ID = r'[a-zA-Z][a-zA-Z_]*'
     ID ['print'] = PRINT 
+    ID ['int'] = INT
 obj = lex()
-expr = input("enter tiny c code");
-for token in obj.tokenize(expr):
-    print(f'token type = {token.type} token value = {token.value}')
