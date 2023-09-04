d98ce847ab14e6f1da80e1eb78ad09e084cd6ae4
bala0000 <aluribalakalki3@gmail.com>
2023-09-04 22:21:13 +0530

    Program file for Parser

---
+++ Program.py
@@ -0,0 +1,20 @@
+class Program:
+	def __init__(self):
+		self.functions = {}		
+	
+	def addFunctionDetails(self,name, function):
+		self.functions[name] = function
+
+	def print(self):
+		for name in self.functions:
+			self.functions[name].print()
+	
+
+	def getMainFunction(self):
+		for funname, function in self.functions.items():
+			if(funname == 'main'):
+				return function
+	# def getFunctionDeatils(self, name):
+	# 	if name in functions:
+	# 		return functions[name]
+	# 
